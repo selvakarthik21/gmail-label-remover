1e40220d7b06aa67988ad5406cfbef07604b306d
Karthik <karthik21@live.in>
2017-11-28 02:40:39 +0530

    Button click & Label list added

--- web/index.php
+++ web/index.php
@@ -29,7 +29,7 @@
 			      	<div class="input-group">
 			      	  <input type="text" class="form-control" placeholder="Enter your Search Query" id="query">
 				      <span class="input-group-btn">
-				        <button class="btn btn-secondary" type="button"  onclick="listMessages();">Go!</button>
+				        <button class="btn btn-secondary" type="button" id="RemoveLabels">Remove Labels!</button>
 				      </span>	     
 				    </div>
 				</div>
@@ -39,43 +39,11 @@
           <tr>
             <th>From</th>
             <th>Subject</th>
-            <th>Date/Time</th>
+            <th>Tagged Labels</th>
           </tr>
         </thead>
         <tbody></tbody>
       </table>
-    </div>
-
-    <div class="modal fade" id="compose-modal" tabindex="-1" role="dialog">
-      <div class="modal-dialog modal-lg">
-        <div class="modal-content">
-          <div class="modal-header">
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-            </button>
-            <h4 class="modal-title">Compose</h4>
-          </div>
-          <form onsubmit="return sendEmail();">
-            <div class="modal-body">
-              <div class="form-group">
-                <input type="email" class="form-control" id="compose-to" placeholder="To" required />
-              </div>
-
-              <div class="form-group">
-                <input type="text" class="form-control" id="compose-subject" placeholder="Subject" required />
-              </div>
-
-              <div class="form-group">
-                <textarea class="form-control" id="compose-message" placeholder="Message" rows="10" required></textarea>
-              </div>
-            </div>
-            <div class="modal-footer">
-              <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-              <button type="submit" id="send-button" class="btn btn-primary">Send</button>
-            </div>
-          </form>
-        </div>
-      </div>
     </div>
 
     <div class="modal fade" id="reply-modal" tabindex="-1" role="dialog">
